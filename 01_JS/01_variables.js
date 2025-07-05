2da5ea2f5bcc5b465bc8edad0e955569bb6a3756
Arman J. <ajcoder09@gmail.com>
2025-07-05 16:25:03 +0530

    Update 01_variables.js

--- 01_JS/01_variables.js
+++ 01_JS/01_variables.js
@@ -4,7 +4,6 @@ var accountPassword = "aa55" // here var is only used for explain. don't use var
 
 let accountState; 
 
- accountEmail = "a9@gmail.com"
  accountEmail = "a9@gmail.com"  
  accountPassword = "6698"
 accountCity = "ahmedabad"

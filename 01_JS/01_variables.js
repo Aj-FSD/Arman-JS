71c682b6165c443a3e3ab0e7c6b6d3bd294fd4f5
Arman J. <ajcoder09@gmail.com>
2025-07-05 15:44:42 +0530

    Create 01_variables.js

---
+++ 01_JS/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = "654645"
+let accountEmail = "aj@gmail.com"
+var accountPassword = "aa55"
+
+let accountState;
+
+ accountEmail = "a9@gmail.com"
+ accountPassword = "6698"
+accountCity = "ahmedabad"
+console.log (accountCity)
+console.table ([ accountEmail, accountId, accountPassword, accountState]);
+// variables  done

95e78ea153da40ae64836030541482cf6ff4af29
Arman J. <ajcoder09@gmail.com>
2025-07-06 20:55:03 +0530

    Create 06_strings.js

---
+++ 01_JS/06_strings.js
@@ -0,0 +1,52 @@
+const name = "Aramn"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Arman-Aj-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+
+
+console.log(gameName.indexOf('m'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-5, 3)
+console.log(anotherString);
+
+const newStringOne = "   Arman    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://Arman.com/Arman%20Jangra"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
+
+const aboutMe = "my name is arman"
+console.log(aboutMe.endsWith("arman"));
+console.log(aboutMe.includes("arman"));
+console.log(aboutMe.replace("arman", "shivam"));
+console.log(aboutMe.search("arman"));
+console.log(aboutMe.startsWith("my"));
+console.log(aboutMe.startsWith("arman"));
+console.log(aboutMe.toUpperCase("arman"));
+console.log(aboutMe.toUpperCase());
+console.log(aboutMe.toLowerCase("arman"));
+console.log(aboutMe.toLowerCase());
+console.log(aboutMe.toLowerCase());
+
+

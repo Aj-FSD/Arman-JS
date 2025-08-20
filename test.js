79259f9585b642ed9249ac0772423d8ce44023a1
Arman J. <ajcoder09@gmail.com>
2025-08-20 23:10:25 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,20 @@
+// function addofsquare (x, y) {
+//     addition = (x*x) + (y*y) 
+//     return addition
+// }
+// console.log(addofsquare(2,3));
+
+function maxno (x,y,z){
+  
+    if (x>y && x>z) {
+        console.log("x is greatest no:",x);
+       
+    } else if (y>x && y>z) {
+        console.log("y is greatest no:",y);
+       
+    } else {
+        console.log("z is greatest no:",z);
+      
+    }
+    }
+    maxno(5,3,2);
